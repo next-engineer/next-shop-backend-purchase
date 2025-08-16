1585cf0995b099b36b549b08d034c88121fde13b
MinDongIl <ehddlf9685@gmail.com>
2025-08-16 15:06:19 +0900

    add DNS Address

--- src/main/java/com/next/app/config/CorsConfig.java
+++ src/main/java/com/next/app/config/CorsConfig.java
@@ -16,7 +16,9 @@ public class CorsConfig {
         CorsConfiguration cfg = new CorsConfiguration();
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                //alb DNS 주소
+                "dong-test-alb-1868547901.ap-northeast-2.elb.amazonaws.com"
                 //프론트엔드 도매인 생성 시 사용.
 //                "https://your-frontend-domain.com"
         ));
