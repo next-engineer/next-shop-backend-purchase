227f7ab5379b0bcf7708e86fb3d5fd236640cc5e
MinDongIl <ehddlf9685@gmail.com>
2025-08-18 13:19:55 +0900

    프론트 주소 허용

--- src/main/java/com/next/app/config/CorsConfig.java
+++ src/main/java/com/next/app/config/CorsConfig.java
@@ -17,10 +17,8 @@ public class CorsConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                //alb DNS 주소
-                "dong-test-alb-1868547901.ap-northeast-2.elb.amazonaws.com"
                 //프론트엔드 도매인 생성 시 사용.
-//                "http://shop.nextcloudlab.com"
+                "https://shop.nextcloudlab.com/"
         ));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization","Content-Type","X-Requested-With"));
